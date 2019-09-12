31effde0c60280328b617b622b5a769d778daa83
spavkov <spavkov@gmail.com>
2019-09-12 16:56:17 +0200

    Initial commit

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome!';
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
