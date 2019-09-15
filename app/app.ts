14f957e42223607ff9258b5e8cb6357b85f5af5b
spavkov <spavkov@gmail.com>
2019-09-15 18:42:06 +0200

    more

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,12 @@
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome!';
+    let playerName: string = 'slobo'
+    logPlayer(playerName);
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame').addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome!';
+    var playerName = 'slobo';
+    logPlayer(playerName);
+}
+function logPlayer(name) {
+    console.log("New game starting for player: " + name);
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
